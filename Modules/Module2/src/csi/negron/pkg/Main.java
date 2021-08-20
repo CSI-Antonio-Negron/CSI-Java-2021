d28b76452b8a96d0c8b3d618e11d95523b548d14
DESKTOP-U5LP4SS\arnvi <25-036@sanignacio.pr>
2021-08-20 09:06:58 -0400

    Hello World Module2

---
+++ Modules/Module2/src/csi/negron/pkg/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package csi.negron.pkg;
+
+/**
+ * @author arnvi
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("Antonio Alejandro Negron Vazquez");
+        System.out.println("25-036");
+	}
+
+}
