5d945618599979605d7b3767791a07921718c8af
DESKTOP-U5LP4SS\arnvi <25-036@sanignacio.pr>
2021-09-01 22:30:03 -0400

    I added the String scientific name to the constructor and added all the information about my animal

---
+++ MainProject/src/csi/negron/pkg/Animal.java
@@ -0,0 +1,63 @@
+package csi.negron.pkg;
+
+public class Animal {
+	// Fields
+	String name;
+	int age;
+	String scientificName;
+	
+    //  Constructors
+	public Animal(String name,int age) {
+		this.name = name;
+		this.age = age;
+	}
+	public Animal() {}
+
+	// Getters and Setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getScientificName() {
+		return scientificName;
+	}
+
+	public void setScientificName(String scientificName) {
+		this.scientificName = scientificName;
+	}
+
+//  Main Method
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		a.setName("Coqui");
+		a.setAge(3);
+		a.setScientificName("Eleutherodactylus coqui");
+
+		System.out.println("Name: " + a.getName());
+		System.out.println("Age: " + a.getAge());
+		System.out.println("Scientific Name: " + a.getScientificName());
+		
+		
+		Animal a2 = new Animal();
+
+
+		System.out.println("Name: " + a.getName());
+		System.out.println("Age: " + a.getAge());
+		System.out.println("Scientific Name: " + a.getScientificName());
+		
+
+	}
+
+}
