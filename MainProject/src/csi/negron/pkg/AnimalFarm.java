06d2ed95464973efe71c628120ebaf4ed6180f03
DESKTOP-U5LP4SS\arnvi <25-036@sanignacio.pr>
2021-09-16 08:29:37 -0400

    Hicimos animal farm y otra forma de hacerle print al taxonomy

--- MainProject/src/csi/negron/pkg/AnimalFarm.java
+++ MainProject/src/csi/negron/pkg/AnimalFarm.java
@@ -10,23 +10,29 @@ public class AnimalFarm {
 	// Main Method
 	public static void main(String[] args) {
 	
-		animals = new Animal[5];
+		animals = new Animal[10];
 
 		animals[0]=new Animal("Coqui",3,"Eleutherodactylus coqui",new Taxonomy("Animal","Chordate","Anura"," Eleutherodactylidae","Common Coqui","Eleuths","Puerto Rican Coqui","Eukaryote"));
 		animals[1]=new Animal("Canadian Goose",13,"Branta Canadensis",new Taxonomy("Animal","Chordate","Anseriformes","Anatidae","Giant","Brent Geese","Giant Canada Goose","Eukaryote"));
 		animals[2]=new Animal("Domestic Pig",16,"Sus Scrofa Domesticus",new Taxonomy("Animal","Chordate","Artiodactyla","Suidae","S.domesticus","Sus","Eukaryote","Sus scrofa domesticus"));
 		animals[3]=new Animal("Sheep",7,"Ovis",new Taxonomy("Animal","Chordate","Ovis","Bovidae","Dorper","Ovis","Stone Sheep","Eukaryote"));
-
-		System.out.println(animals[0]);
-		System.out.println();
-		System.out.println(animals[1]);
-		System.out.println();
-		System.out.println(animals[2]);
-		System.out.println();
-		System.out.println(animals[3]);
-		System.out.println(animals[4]);
+        animals[4]=new Animal();
+        animals[5]=new Animal("Bornean Bearded Pig",5,"Sus Barbatus");
+        animals[6]=new Animal("Bighorn Sheep",9,"Ovis Canadensis", new Taxonomy());
 		
-		
+		printAnimals();
 		
 	} 
-}
+	
+	public static void printAnimals() {
+		
+		for(int i = 0;i<animals.length;i++) {
+			
+			System.out.println("Animal: " + i);
+			
+			System.out.println(animals[i]);
+		}
+		
+	}
+	}
+
